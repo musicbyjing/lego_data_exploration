cf904a2cbcfdaac6c9ea41aaf516039a0bd7c838
musicbyjing <musicbyjing@gmail.com>
2020-08-06 21:52:14 -0400

    Begin sequential model

--- Lego_data.py
+++ Lego_data.py
@@ -9,7 +9,7 @@
 # 
 # 
 
-# In[220]:
+# In[169]:
 
 
 import pandas as pd
@@ -28,7 +28,7 @@ plt.style.use('fivethirtyeight')
 #     - Does this align with inflation? (Pick a specific currency)
 # - Are some themes more expensive than others? (Pick a specific year range)
 
-# In[232]:
+# In[170]:
 
 
 curr_themes = ['Architecture', 'Brick Sketches', 'BrickHeadz', 'City', 'Classic',                'Collectable Minifigures', 'Creator', 'Creator Expert', 'DC Comics Super Heroes'                 'Disney', 'DOTS', 'Duplo', 'Education', 'Friends', 'Harry Potter',                'Hidden Side', 'Ideas', 'Jurassic World', 'LEGO Art', 'Marvel Super Heroes',                'Mindstorms', 'Minecraft', 'Minions: The Rise of Gru', 'Monkie Kid', 'Ninjago'                'Overwatch', 'Powered Up', 'Speed Champions', 'Star Wars', 'Super Heroes',                'Super Mario', 'Technic ', 'Trolls World Tour']
@@ -41,7 +41,7 @@ data.head()
 
 # ## Visualizing numerical values: Year, Minifigs, Pieces, USRetailPrice
 
-# In[233]:
+# In[171]:
 
 
 fig, axes = plt.subplots(nrows = 2, ncols = 2)
@@ -60,7 +60,7 @@ fig.set_size_inches(20, 10)
 
 # A large amount of entries have no pieces (`NaN` or `0`, from looking at the CSV). Either they are not sets (e.g. books, pens, video games, etc.) or the data is incomplete. Regardless, we will discard them.
 
-# In[234]:
+# In[172]:
 
 
 data = data[~(pd.isna(data['Pieces']) | (data['Pieces']==0))]
@@ -69,7 +69,7 @@ print('Table has shape:', data.shape)
 
 # ## Visualizing categorical values: ThemeGroup
 
-# In[235]:
+# In[173]:
 
 
 ax = data.groupby('ThemeGroup').size().sort_values().plot(kind='barh', figsize=(10,7))
@@ -78,27 +78,18 @@ ax.set_xlabel('Count')
 
 # ## Are the numerical values correlated?
 
-# In[236]:
+# In[175]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-from yellowbrick.features import Rank2D
-from yellowbrick.style import set_palette
-
-X = data[num_features].dropna().to_numpy()
-
-visualizer = Rank2D(features=num_features, algorithm='pearson')
-visualizer.fit(X)
-visualizer.transform(X)
-visualizer.show()
+import seaborn as sns
+sns.pairplot(data[["Year", "Minifigs", "Pieces", "USRetailPrice"]], diag_kind="kde")
 
 
-# A few things of note (that should be prior knowledge for Lego fans!):
-# - Number of pieces is strongly correlated with price
-# - Larger, more expensive sets are more likely to have more minifigures
+# The most striking correlation is that the number of pieces is strongly correlated with price. In other words, larger sets tend to be more expensive.
 # 
-# More interestingly, there is a weak trend of sets getting bigger and more expensive over the years. Let's use the data to confirm this hypothesis.
+# More interestingly, there is a trend of sets getting bigger and more expensive over the years. Let's use the data to confirm this hypothesis.
 
 # ## Has price per piece increased over time?
 # 
@@ -106,7 +97,7 @@ visualizer.show()
 # 
 # Since the first Lego themes were introduced in 1978, we must consider inflation--\\$1 in 1978 is worth more than \\$1 in 2020. Luckily, I found a [CSV file](https://www.in2013dollars.com/us/inflation/1978?amount=1) that lets us account for this.
 
-# In[237]:
+# In[176]:
 
 
 year_list = list(range(1978, 2021)) # The first Lego themes were introduced in 1978
@@ -131,7 +122,7 @@ plt.figure(figsize=(10,7))
 plt.xlabel('Year')
 plt.ylabel('Price per piece [$USD]')
 plt.title('Year vs. Price (US Retail) per Piece')
-plt.plot(year_list, ppp_list, label='Average')
+plt.plot(year_list, ppp_list, label='Average', linewidth=4)
 
 
 # The overall trend seems to be increasing. 
@@ -139,7 +130,7 @@ plt.plot(year_list, ppp_list, label='Average')
 # But some theme groups, such as Licensed, have a reputation in the Lego community for being more expensive than others. Let's see if this is true.
 # 
 
-# In[238]:
+# In[177]:
 
 
 # theme_groups = ['Licensed', 'Miscellaneous', 'Modern day', 'Pre-school', 'Action/Adventure', \
@@ -165,7 +156,7 @@ plt.figure(figsize=(10,7))
 plt.xlabel('Year')
 plt.ylabel('Price per piece [$USD]')
 plt.title('Year vs. Price (US Retail) per Piece')
-plt.plot(year_list, ppp_list, label='Average')
+plt.plot(year_list, ppp_list, label='Average', linewidth=4)
 plot_ppp_list("Licensed")
 plt.legend()
 
@@ -174,7 +165,7 @@ plt.legend()
 # 
 # Let's plot a few more theme groups to compare.
 
-# In[239]:
+# In[178]:
 
 
 plt.figure(figsize=(10,7)) 
@@ -191,7 +182,7 @@ plt.legend()
 
 # What's responsible for the spike in Miscellaneous at 1997?
 
-# In[229]:
+# In[179]:
 
 
 temp = data[(data['ThemeGroup'] == "Miscellaneous") & (data['Year'] == 1997)]
@@ -202,18 +193,20 @@ temp[['SetID', 'Theme', 'Year', 'Name', 'Pieces', 'USRetailPrice']]
 # 
 # It isn't really a "set", per se, so given this and similar occurrences, it might be best to exclude the Miscellaneous theme entirely.
 
-# In[268]:
+# In[180]:
 
 
 data = data[~(data['ThemeGroup'] == 'Miscellaneous')]
 print('Table has shape:', data.shape)
 
 
-# #### Price prediction using year only
-# 
-# We'll first plot a regression line and see how linear the data is.
+# ## Price prediction models
 
-# In[266]:
+# ### Regression using Year only
+# 
+# We'll first try linear reression.
+
+# In[183]:
 
 
 from sklearn.linear_model import LinearRegression
@@ -225,12 +218,12 @@ model = LinearRegression().fit(x, y)
 y_pred = model.predict(x)
 
 r_sq = model.score(x,y)
-print('Coefficient of determination r-squared:', r_sq)
+r_sq
 
 
-# The $r^{2}$ value is 0.79, which indicates a decent fit to the data. However, the price has been dropping in recent years, and there are peaks and troughs throughout the data. While we can say that the price generally increases in the long run, this simple model would fall short in making short-term predictions.
+# The r^2 value is 0.79, which indicates a decent fit to the data. However, the price has been dropping in recent years, and there are peaks and troughs throughout the data. While we can say that the price generally increases in the long run, this simple model would fall short in making short-term predictions.
 
-# In[272]:
+# In[182]:
 
 
 plt.figure(figsize=(10,7)) 
@@ -241,29 +234,137 @@ plt.plot(year_list, ppp_list, label='Average')
 plt.plot(x, y_pred, linewidth=4)
 
 
-# In[ ]:
+# ### Regression using Year, Pieces, Minifigs, Theme
+
+# In[136]:
 
 
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
 
 
-
-# In[ ]:
-
+# In[137]:
 
 
+data['Theme'].isnull().values.any()
 
 
-# In[ ]:
+# In[138]:
 
 
+temp = data[['Year', 'Minifigs', 'Pieces','USRetailPrice']]
+temp2 = pd.get_dummies(data['Theme']) # Turn Theme into a dummy variable
+data_new = pd.concat([temp, temp2], axis=1)
+# Drop NaN's and 0's for price
+data_new = data_new[data_new['USRetailPrice'].notna() & (data_new['USRetailPrice'] != 0)] 
+data_new = data_new.replace(np.nan, 0)
 
 
-
-# In[ ]:
-
+# In[139]:
 
 
+# Separate into training and testing sets
+train_dataset = data_new.sample(frac=0.8,random_state=0)
+test_dataset = data_new.drop(train_dataset.index)
+train_stats = train_dataset.describe()
+train_stats.pop('USRetailPrice')
+train_stats = train_stats.transpose()
+train_stats
 
+
+# The features have significantly different ranges, so we should normalize them to avoid dependence on our choice of unit. 
+# 
+# 
+
+# In[140]:
+
+
+# def norm(x):
+#     return (x - train_stats['mean']) / train_stats['std']
+# normed_train_data = norm(train_dataset)
+# normed_test_data = norm(test_dataset)
+
+
+# In[141]:
+
+
+normed_train_data = train_dataset
+normed_test_data = test_dataset
+
+
+# In[142]:
+
+
+train_labels = train_dataset.pop('USRetailPrice')
+test_labels = test_dataset.pop('USRetailPrice')
+
+
+# In[143]:
+
+
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1)
+    ])
+
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+    model.compile(loss='mse',
+        optimizer='adam',
+        metrics=['mae', 'mse'])
+    return model
+
+model = build_model()
+model.summary()
+
+
+# In[145]:
+
+
+EPOCHS = 500
+
+history = model.fit(
+    normed_train_data, train_labels,
+    epochs=EPOCHS, validation_split = 0.2, verbose=1,
+#     callbacks=[tfdocs.modeling.EpochDots()]
+)
+
+
+# In[157]:
+
+
+plotter = tfdocs.plots.HistoryPlotter(smoothing_std=2)
+plotter.plot({'Basic': history}, metric = "mse")
+plt.ylim([0, 1000])
+plt.ylabel('MSE [USRetailPrice]')
+
+
+# In[160]:
+
+
+plotter.plot({'Basic': history}, metric = "mae")
+plt.ylim([0, 30])
+plt.ylabel('MAE [USRetailPrice]')
+
+
+# In[166]:
+
+
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
+mae
+
+
+# Issues:
+# - MAE is \\$9.32 after one run, which is pretty high
+# - Normalizing doesn't seem to work
+# - Separate data exploration and model building into two notebooks, if GH pages allows
 
 # In[ ]:
 
